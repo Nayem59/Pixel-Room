dd8e84363bac8b9f06ed6d9e21c2bac6de864892
Nayem59 <Nayem.Alam97@gmail.com>
2024-12-22 19:29:19 +0000

    added express and created basic express/socket io server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
-console.log("hi from server!!!");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const PORT = 3000;
+
+// Serve static files
+app.use(express.static("app"));
+
+// Handle Socket.IO connections
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
