36285c37a8febd3ca89ab14392dad84fd255441b
Nayem59 <Nayem.Alam97@gmail.com>
2024-12-23 21:10:25 +0000

    front end handle new room creation and adding to room list

--- app/main.js
+++ app/main.js
@@ -1,19 +1,56 @@
-const socket = io("http://localhost:3000"); // Connect to your server
+// const socket = io("http://localhost:3000"); // Connect to your server
 
-// Handle connection
-socket.on("connect", () => {
-  console.log("Connected to the server with ID:", socket.id);
+const newRoomForm = document.querySelector(".new-room-form");
+const newRoomInput = document.querySelector(".new-room-input");
+const roomContainer = document.querySelector(".room-container");
+
+let roomName = null;
+let roomNames = [];
+
+newRoomForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  roomName = newRoomInput.value.replace(/\s/g, "");
+  if (!roomName) {
+    console.log("name empty");
+    roomName = null;
+    return;
+  }
+  if (roomNames.includes(roomName)) {
+    console.log("room name already exist");
+    roomName = null;
+    return;
+  }
+
+  roomNames.push(roomName);
+  newRoomInput.value = "";
+  const newDiv = document.createElement("div");
+  newDiv.innerText = roomName;
+  roomName = null;
+  newDiv.className = "room";
+
+  newDiv.addEventListener("click", (event) => {
+    const roomName = event.target.innerText;
+    console.log(`You clicked on Room ${roomName}`);
+  });
+
+  roomContainer.appendChild(newDiv);
 });
 
-// Handle messages from the server
-socket.on("message", (msg) => {
-  console.log("Message from server:", msg);
-});
+// // Handle connection
+// socket.on("connect", () => {
+//   console.log("Connected to the server with ID:", socket.id);
+// });
 
-// Handle disconnection
-socket.on("disconnect", () => {
-  console.log("Disconnected from the server");
-});
+// // Handle messages from the server
+// socket.on("message", (msg) => {
+//   console.log("Message from server:", msg);
+// });
 
-// Emit a message to the server
-socket.emit("message", "Hello, a message from the client");
+// // Handle disconnection
+// socket.on("disconnect", () => {
+//   console.log("Disconnected from the server");
+// });
+
+// // Emit a message to the server
+// socket.emit("message", "Hello, a message from the client");
